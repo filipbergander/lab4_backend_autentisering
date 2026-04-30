ec6940bf1840cd54e4f27336a92754156c785f0b
Filip Bergander <filip.bergander1996@gmail.com>
2026-04-30 08:37:52 +0200

    Lagt till cors och välkomstmeddelande till webbtjänsten.

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express'); // Expresspaket
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken'); // JWT för tokens
 require('dotenv').config(); // För att använda .env-filen
+const cors = require('cors'); // För att ansluta till servern från annan domän
 const port = process.env.PORT | 3000;
 
 const app = express();
 app.use(bodyParser.json());
+
+
+// Routes
+app.get("/", async(req, res) => {
+    res.json("Välkommen till webbtjänsten!")
+});
 
 // Startar applikationen
 app.listen(port, () => {
